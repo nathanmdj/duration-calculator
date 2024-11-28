e3bc523209d58947acd935e67e26b4fe1f004658
nathanmdj <nathanmdj628@gmail.com>
2024-11-28 16:57:10 +0800

    added posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css"
 import { Metadata } from "next"
+import dynamic from "next/dynamic"
 
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
@@ -7,6 +8,12 @@ import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+
+import { PHProvider } from "./provider"
+
+const PostHogPageView = dynamic(() => import("./PostHogPageView"), {
+  ssr: false,
+})
 
 export const metadata: Metadata = {
   title: {
@@ -34,20 +41,25 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <>
       <html lang="en" suppressHydrationWarning>
         <head />
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-        >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
-              <div className="flex-1">{children}</div>
-            </div>
-            <TailwindIndicator />
-          </ThemeProvider>
-        </body>
+        <PHProvider>
+          <body
+            className={cn(
+              "min-h-screen bg-background font-sans antialiased",
+              fontSans.variable
+            )}
+          >
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              <div className="relative flex min-h-screen flex-col">
+                <SiteHeader />
+                <div className="flex-1">
+                  <PostHogPageView />
+                  {children}
+                </div>
+              </div>
+              <TailwindIndicator />
+            </ThemeProvider>
+          </body>
+        </PHProvider>
       </html>
     </>
   )
